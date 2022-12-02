1070fc59e498fde69b2ac87953ea2b28266d507f
Chris Kamphuis <mail@chriskamphuis.com>
2022-12-02 11:14:44 +0100

    day 2

--- 02/2.py
+++ 02/2.py
@@ -6,6 +6,5 @@ with open('input.txt', 'r') as f:
         diff = {'X': -1, 'Y': 0, 'Z': 1}
         back = {0: 'X', 1: 'Y', 2: 'Z'} 
         s = back[(forw[o]+diff[y])%3]
-        score += (diff[y]+1) * 3
-        score += (diff[s]+1) + 1
+        score += (diff[y]+1) * 3 + (diff[s]+1) + 1
 print(score)
