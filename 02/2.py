a4abbbc46b842eb3394831e9b597785404a72db3
Chris Kamphuis <mail@chriskamphuis.com>
2022-12-02 11:24:28 +0100

    day 2

--- 02/2.py
+++ 02/2.py
@@ -1,6 +1,6 @@
 score = 0
+mp = {'A': 0, 'B': 1, 'C': 2, 'X': -1, 'Y': 0, 'Z': 1}  
+back = {0: 'X', 1: 'Y', 2: 'Z'} 
 for o, y in [line.strip().split() for line in open('input.txt')]:
-    mp = {'A': 0, 'B': 1, 'C': 2, 'X': -1, 'Y': 0, 'Z': 1}  
-    back = {0: 'X', 1: 'Y', 2: 'Z'} 
     score += (mp[y]+1) * 3 + (mp[back[(mp[o]+mp[y])%3]]+1) + 1
 print(score)
