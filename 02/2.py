899fe57d38a7320d12ca44315bac050a40b37237
Chris Kamphuis <mail@chriskamphuis.com>
2022-12-02 11:22:59 +0100

    day 2

--- 02/2.py
+++ 02/2.py
@@ -1,8 +1,6 @@
 score = 0
-with open('input.txt', 'r') as f:
-    for line in f:
-        o, y = line.strip().split(' ')
-        mp = {'A': 0, 'B': 1, 'C': 2, 'X': -1, 'Y': 0, 'Z': 1}  
-        back = {0: 'X', 1: 'Y', 2: 'Z'} 
-        score += (mp[y]+1) * 3 + (mp[back[(mp[o]+mp[y])%3]]+1) + 1
+for o, y in [line.strip().split() for line in open('input.txt')]:
+    mp = {'A': 0, 'B': 1, 'C': 2, 'X': -1, 'Y': 0, 'Z': 1}  
+    back = {0: 'X', 1: 'Y', 2: 'Z'} 
+    score += (mp[y]+1) * 3 + (mp[back[(mp[o]+mp[y])%3]]+1) + 1
 print(score)
