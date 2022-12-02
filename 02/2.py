c44a6a7455158f059bbd65da6062819f48a360b1
Chris Kamphuis <mail@chriskamphuis.com>
2022-12-02 11:12:52 +0100

    day 2

--- 02/2.py
+++ 02/2.py
@@ -2,18 +2,10 @@ score = 0
 with open('input.txt', 'r') as f:
     for line in f:
         o, y = line.strip().split(' ')
-        if y == 'Z':
-            score += 6
-        if y == 'Y':
-            score += 3
         forw = {'A': 0, 'B': 1, 'C': 2}  
         diff = {'X': -1, 'Y': 0, 'Z': 1}
         back = {0: 'X', 1: 'Y', 2: 'Z'} 
         s = back[(forw[o]+diff[y])%3]
-        if s == 'X':
-            score += 1
-        if s == 'Y':
-            score += 2
-        if s == 'Z': 
-            score += 3
+        score += (diff[y]+1) * 3
+        score += (diff[s]+1) + 1
 print(score)
